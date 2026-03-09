388a83d87f890942bd77b034dd6397c765399846
Natanael Cristofe Dev <cristofegit@gmail.com>
2026-03-09 10:37:07 -0300

    Add files via upload

---
+++ Aula02/tipos.js
@@ -0,0 +1,25 @@
+
+let nome = "João";      
+let idade = 20;         
+let ativo = true;       
+
+// Mostrar o tipo das variáveis
+console.log("Tipo de nome:", typeof nome);
+console.log("Tipo de idade:", typeof idade);
+console.log("Tipo de ativo:", typeof ativo);
+
+
+let textoNumero = "15";
+let numeroConvertido = Number(textoNumero);
+console.log("String para número:", numeroConvertido, typeof numeroConvertido);
+
+
+let numero = 30;
+let numeroParaString = String(numero);
+console.log("Número para string:", numeroParaString, typeof numeroParaString);
+
+
+let numeroBoolean = 1;
+let numeroParaBoolean = Boolean(numeroBoolean);
+console.log("Número para boolean:", numeroParaBoolean, typeof numeroParaBoolean);
+
